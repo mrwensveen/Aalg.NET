85fa84b2e66e0e239fe741c5f7d4a7277cb18b7f
Matthijs Wensveen <matthijs.wensveen@gmail.com>
2011-12-27 15:13:28 +0100

    first commit

---
+++ Aalg.NET/Generator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Aalg.NET
+{
+    public class Generator
+    {
+        private readonly Random random = new Random();
+        private IList<string[]> phrases;
+        private string newLine;
+
+        public Generator(Stream input, string newLine)
+        {
+            this.newLine = newLine;
+            InitializePhrases(input);
+        }
+
+        private void InitializePhrases(Stream input)
+        {
+            TextReader regels = new StreamReader(input);
+
+            phrases = new List<string[]>();
+            string[] phrase;
+            while ((phrase = ReadPhrase(regels)) != null)
+            {
+                // Add the phrase to the list
+                phrases.Add(phrase);
+            }
+        }
+
+        public string GetRandomPhrasePart(int part)
+        {
+            return phrases[random.Next(phrases.Count)][part];
+        }
+
+        public string GenerateStrophe()
+        {
+            return String.Format("{0} {1}{5}{2} {3} {4}{5}",
+                GetRandomPhrasePart(0), GetRandomPhrasePart(1),
+                GetRandomPhrasePart(0), GetRandomPhrasePart(1), GetRandomPhrasePart(2),
+                newLine);
+        }
+
+        /// <summary>
+        /// Read a phrase (consisting of 3 parts)
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        private string[] ReadPhrase(TextReader reader)
+        {
+            string[] phrase = new string[3];
+            for (int i = 0; i < 3; i++)
+            {
+                if ((phrase[i] = reader.ReadLine()) == null)
+                {
+                    return null;
+                }
+            }
+
+            return phrase;
+        }
+    }
+}
---
+++ AalgWin/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Aalg.NET;
+using System.IO;
+
+namespace AalgWin
+{
+	public partial class Form1 : Form
+	{
+		private readonly Generator generator;
+		public Form1()
+		{
+			string[] args = Environment.GetCommandLineArgs();
+
+			string filename = args.Length > 1 ? args[1] : "regels.dat";
+			Stream regels = new FileInfo(filename).OpenRead();
+
+			generator = new Generator(regels, "\r\n");
+
+
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			this.Font = new Font(this.Font.FontFamily, 12f);
+			button1.Text = RenderLyrics();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			textBox1.Clear();
+			button1.Text = RenderLyrics();
+		}
+
+		private string RenderLyrics()
+		{
+			// Select and output a title
+			string title = generator.GetRandomPhrasePart(1).ToUpper();
+			WriteLine(textBox1, title + "\r\n");
+
+			// Generate 2 strophes
+			for (int i = 0; i < 2; i++)
+			{
+				WriteLine(textBox1, generator.GenerateStrophe());
+			}
+
+			// Exclamation!
+			WriteLine(textBox1, String.Format("{0} !!\r\n", generator.GetRandomPhrasePart(1)));
+
+			// A final strophe
+			WriteLine(textBox1, generator.GenerateStrophe());
+
+			// More exclamations!
+			WriteLine(textBox1, String.Format("{0} !!", generator.GetRandomPhrasePart(1)));
+			WriteLine(textBox1, String.Format("{0} !!!!\r\n", generator.GetRandomPhrasePart(1)));
+
+			return title;
+		}
+
+		private void WriteLine(TextBox textBox, string line)
+		{
+			textBox.AppendText(line);
+			textBox.AppendText("\r\n");
+		}
+	}
+}
